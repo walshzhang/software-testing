75004daa3f4edfd50ca1759af84568df7224378c
walsh <walsh.zhang@hotmail.com>
2020-04-08 15:18:44 +0800

    mockito docs

--- tools-mockito/src/main/java/iteach/softwaretesting/tools/mockito/Car.java
+++ tools-mockito/src/main/java/iteach/softwaretesting/tools/mockito/Car.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
+package iteach.softwaretesting.tools.mockito;
 
 public interface Car {
+    boolean needsFuel();    // 是否需要加油
+    double getEngineTemperature();     // 获取发动机温度
+    void driveTo(String destination);  // 驶往某地
 }
--- tools-mockito/src/test/java/iteach/softwaretesting/tools/mockito/CarTest.java
+++ tools-mockito/src/test/java/iteach/softwaretesting/tools/mockito/CarTest.java
@@ -1,4 +1,17 @@
 package iteach.softwaretesting.tools.mockito;
 
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.*;
+
 public class CarTest {
+    private final Car ferrari = mock(Car.class);
+
+    @Test
+    public void testIfCarIsACar() {
+        assumeTrue(ferrari != null);
+        assertTrue(ferrari instanceof Car);
+    }
 }
